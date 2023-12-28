089bb83e21d79e49a002821d52a66e597c6c2c63
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-28 06:15:11 +0200

    init project

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by OLEKSANDR ISAIEV on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
