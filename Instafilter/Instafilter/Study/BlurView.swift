789393c13bb225f335d8cd0810669f0051c4b099
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-28 06:52:10 +0200

    responding to state changes using onChange

--- Instafilter/Instafilter/Study/BlurView.swift
+++ Instafilter/Instafilter/Study/BlurView.swift
@@ -14,6 +14,9 @@ struct BlurView: View {
         Text("Hello, World!")
             .blur(radius: blurAmount)
         Slider(value: $blurAmount, in: 0...20)
+            .onChange(of: blurAmount) { oldValue, newValue in
+            print("New value is \(newValue)")
+            }
         Button("Random Blur") {
             blurAmount = Double.random(in: 0...20)
         }
