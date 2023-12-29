ec62e36c6c516f241be17156813c89d8130eb049
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-29 14:29:30 +0200

    play with filters settings

--- Instafilter/Instafilter/Study/CoreImageView.swift
+++ Instafilter/Instafilter/Study/CoreImageView.swift
@@ -31,8 +31,19 @@ struct CoreImageView: View {
 
         currentFilter.inputImage = beginImage
 //        currentFilter.intensity = 1
-        currentFilter.scale = 20
+        let amount = 1.0
+//        currentFilter.scale = 20
         
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
         
