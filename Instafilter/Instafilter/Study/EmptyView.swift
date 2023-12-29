3b72d47674185fb25d6b62328ee69a4037ba5a00
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-29 14:36:43 +0200

    prepare empty view

---
+++ Instafilter/Instafilter/Study/EmptyView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyView.swift
+//  Instafilter
+//
+//  Created by OLEKSANDR ISAIEV on 29.12.2023.
+//
+
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyView()
+}
