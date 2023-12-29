4258b0b7c325a38d1adc8aee7b9a5ab7e5a6ab53
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-29 14:42:36 +0200

    implement default empty view with custom messages

--- Instafilter/Instafilter/Study/EmptyView.swift
+++ Instafilter/Instafilter/Study/EmptyView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct EmptyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ContentUnavailableView("No smippets",
+                               systemImage: "swift",
+                               description: Text("You don't have any saved snippets yet"))
     }
 }
 
