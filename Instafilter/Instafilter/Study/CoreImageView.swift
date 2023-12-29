569304f7c282b9a0c847a7fb5074ae68d63714c4
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-29 14:13:53 +0200

    pixel example

--- Instafilter/Instafilter/Study/CoreImageView.swift
+++ Instafilter/Instafilter/Study/CoreImageView.swift
@@ -26,9 +26,12 @@ struct CoreImageView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
+//        let currentFilter = CIFilter.sepiaTone()
+        let currentFilter = CIFilter.pixellate()
+
         currentFilter.inputImage = beginImage
-        currentFilter.intensity = 1
+//        currentFilter.intensity = 1
+        currentFilter.scale = 20
         
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
