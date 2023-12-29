484ad6c688da00de0490cdc8b09367e3f3741b1f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-29 13:31:52 +0200

    implement sepia filter

--- Instafilter/Instafilter/Study/CoreImageView.swift
+++ Instafilter/Instafilter/Study/CoreImageView.swift
@@ -5,6 +5,8 @@
 //  Created by OLEKSANDR ISAIEV on 29.12.2023.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct CoreImageView: View {
@@ -20,7 +22,19 @@ struct CoreImageView: View {
     }
     
     private func loadImage() {
-        image = Image(.example)
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
