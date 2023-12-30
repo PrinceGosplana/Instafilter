3c4b1dad2a087f55821bbe64ec4330dbf56f3d8f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-30 15:20:42 +0200

    add share content view empty view

---
+++ Instafilter/Instafilter/Study/ShareContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareContentView.swift
+//  Instafilter
+//
+//  Created by OLEKSANDR ISAIEV on 30.12.2023.
+//
+
+import SwiftUI
+
+struct ShareContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareContentView()
+}
