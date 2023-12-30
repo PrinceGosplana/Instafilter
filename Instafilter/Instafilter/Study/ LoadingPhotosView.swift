f0c02c7ce373aa2ce6ae0d67fe77d390605e38ee
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-30 06:51:07 +0200

    Loading photos from the user's photo library new view

---
+++ Instafilter/Instafilter/Study/ LoadingPhotosView.swift
@@ -0,0 +1,18 @@
+//
+//   LoadingPhotosView.swift
+//  Instafilter
+//
+//  Created by OLEKSANDR ISAIEV on 30.12.2023.
+//
+
+import SwiftUI
+
+struct _LoadingPhotosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _LoadingPhotosView()
+}
