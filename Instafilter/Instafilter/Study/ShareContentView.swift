a0d99bbdf20435c9058a7e84129f6622a2a314b6
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-31 06:38:41 +0200

    share image from the app

--- Instafilter/Instafilter/Study/ShareContentView.swift
+++ Instafilter/Instafilter/Study/ShareContentView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct ShareContentView: View {
     var body: some View {
+        /*
         ShareLink(item: URL(string: "https://www.hackingwithswift.com")!,
                   subject: Text("Learn Swift here"),
                   message: Text("Check out the 100 days of SwiftUI")) {
             Label("Spread the word about Swift", systemImage: "swift")
+        }
+         */
+        
+        let example = Image(.example)
+        
+        ShareLink(item: example, preview: SharePreview("Airport", image: example)) {
+            Label("Click to share", systemImage: "airplane")
         }
     }
 }
