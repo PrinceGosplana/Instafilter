3464d8e978b3a282389a121363a109c834d12272
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-31 06:57:40 +0200

    add empty state and prepare view for better solutions

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -15,21 +15,31 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
                 Spacer()
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
                 }
                 HStack {
-                    Button("Change filter") {
-                        
-                    }
+                    Button("Change filter", action: changeFilter) 
                     Spacer()
                 }
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
         }
+    }
+    
+    private func changeFilter() {
+        
     }
 }
 
