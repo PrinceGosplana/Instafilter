6df4c973418f6ff5130037a601f40709868b8b49
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-31 06:31:03 +0200

    add more information to share link

--- Instafilter/Instafilter/Study/ShareContentView.swift
+++ Instafilter/Instafilter/Study/ShareContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ShareContentView: View {
     var body: some View {
-        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!)
+        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!,
+                  subject: Text("Learn Swift here"),
+                  message: Text("Check out the 100 days of SwiftUI"))
     }
 }
 
