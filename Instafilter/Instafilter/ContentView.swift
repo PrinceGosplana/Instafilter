602f4445353c7ba3687510cacf1529a64f1c6a30
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-01 08:27:34 +0200

    add photos picker in the project

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,23 +5,27 @@
 //  Created by OLEKSANDR ISAIEV on 28.12.2023.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
     
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
                 
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
                 }
                 Spacer()
                 HStack {
@@ -29,7 +33,7 @@ struct ContentView: View {
                     Slider(value: $filterIntensity)
                 }
                 HStack {
-                    Button("Change filter", action: changeFilter) 
+                    Button("Change filter", action: changeFilter)
                     Spacer()
                 }
             }
